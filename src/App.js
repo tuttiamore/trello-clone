d492367ffb6bf63ba50644a41e892535ed8bd043
Franz <franz.hanschmidt@posteo.de>
2021-08-26 10:16:20 +0200

    add background card, add card, add logic for dragndrop between stacks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,104 @@
-import logo from "./logo.svg";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 import BackgroundCard from "./components/BackgroundCard";
 
+const itemsDefault = [
+  {
+    itemId: nanoid(),
+    index: 1,
+    text: "test item 1",
+    checked: false,
+    stackId: "todo",
+  },
+  {
+    itemId: nanoid(),
+    index: 2,
+    text: "test item 2",
+    checked: false,
+    stackId: "doing",
+  },
+  {
+    itemId: nanoid(),
+    index: 3,
+    text: "test item 3",
+    checked: false,
+    stackId: "done",
+  },
+];
+
+const stacksDefault = [
+  {
+    heading: "New list",
+    stackId: "todo",
+  },
+  {
+    heading: "New list",
+    stackId: "doing",
+  },
+  {
+    heading: "New list",
+    stackId: "done",
+  },
+];
+
 function App() {
+  const [items, setItems] = useState([]);
+  const [stacks, setStacks] = useState(stacksDefault);
+
+  const handleAddItem = (stackId) => {
+    let lastItem;
+
+    if (items.length === 0) {
+      lastItem = 0;
+    } else {
+      lastItem = items.reduce((acc, curr) => {
+        acc = curr.index > acc ? curr.index : acc;
+        return acc;
+      }, 0);
+    }
+
+    setItems([
+      ...items,
+      {
+        itemId: nanoid(),
+        index: lastItem + 1,
+        checked: false,
+        text: "",
+        stackId: stackId,
+      },
+    ]);
+  };
+
+  const handleDropWithinStack = (itemId, stackId) => {};
+
+  const handleDropBetweenStacks = (itemId, stackId) => {
+    const updatedStackId = items.map((item) => {
+      if (item.itemId === itemId) item.stackId = stackId;
+      return item;
+    });
+    setItems(updatedStackId);
+  };
+
+  const handleDropItem = (itemId, stackId) => {
+    // Two cases: item being dropped in different place within stack
+    // Item dropped in different stack
+    handleDropBetweenStacks(itemId, stackId);
+  };
+
   return (
-    <div className="App">
-      <BackgroundCard>
-        <p>Hello from BackgroundCard</p>
-      </BackgroundCard>
+    <div className="flex flex-row justify-between">
+      {stacks.map((stack) => {
+        return (
+          <BackgroundCard
+            handleAddItem={handleAddItem}
+            handleDropItem={handleDropItem}
+            stackId={stack.stackId}
+            items={items}
+            key={stack.stackId}
+          ></BackgroundCard>
+        );
+      })}
     </div>
   );
 }
--- src/components/BackgroundCard.js
+++ src/components/BackgroundCard.js
@@ -1,12 +1,48 @@
 import React from "react";
 import AddIcon from "@material-ui/icons/Add";
+import Card from "./Card";
+import { useDrop } from "react-dnd";
+import { ItemTypes } from "../dragndrop/ItemTypes";
 
-export default function BackgroundCard({ children }) {
+export default function BackgroundCard({
+  items,
+  stackId,
+  handleAddItem,
+  handleDropItem,
+}) {
+  const [{ canDrop, isOver }, drop] = useDrop(() => ({
+    accept: ItemTypes.ITEM,
+    drop: () => ({ name: stackId }),
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
+  }));
+
   return (
-    <section className="bg-gray-200 rounded m-2">
-      {children}
-      <article className="flex flex-row">
-        <AddIcon></AddIcon> <p>Add another card</p>
+    <section className="bg-gray-100 rounded m-4 p-4 flex-auto" ref={drop}>
+      {items &&
+        items.map((item) => {
+          if (item.stackId === stackId) {
+            return (
+              <Card
+                key={item.itemId}
+                text={item.text}
+                itemId={item.itemId}
+                items={items}
+                handleDropItem={handleDropItem}
+              ></Card>
+            );
+          }
+          return null;
+        })}
+      <article
+        className="flex flex-row cursor-pointer hover:bg-gray-200"
+        onClick={() => {
+          handleAddItem(stackId);
+        }}
+      >
+        <AddIcon></AddIcon> <p>Add new card</p>
       </article>
     </section>
   );
